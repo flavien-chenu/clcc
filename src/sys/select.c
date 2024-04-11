db5dd0d81da0070c50aa31a66d658f7498157f82
Yann <yannmasson17@gmail.com>
2024-04-11 19:20:41 +0200

    feat(select): add select in Makefile

---
+++ src/sys/select.c
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2024
+** libc-calls-tester
+** File description:
+** select
+*/
+
+#include "internal.h"
+
+#include <sys/select.h>
+
+DECL_CLCC_ARGS_5(int, select, int, fd_set *, fd_set *, fd_set *, struct timeval *)
