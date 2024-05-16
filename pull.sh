87c483cbe811fdabad8a5db55911a81062646d1b
Flavien Chenu <flavienchenu@hotmail.com>
2024-05-16 16:12:14 +0200

    refactor: remove .gitignore of deleted pulled files

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-gitfiles=(.git .gitmodules .gitattributes .gitignore)
+gitfiles=(.git .gitmodules .gitattributes)
 tmp_dir="/tmp/$(uuidgen)"
 repo_url=git@github.com:flavien-chenu/clcc.git
 
