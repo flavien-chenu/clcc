f1889a4e7ca3545f8f62dd2b241cc4d43d1c65f7
Flavien Chenu <flavienchenu@hotmail.com>
2024-05-16 16:40:03 +0200

    feat(sys): add msg module

---
+++ modules/sys/msg.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2024
+** libc-calls-tester
+** File description:
+** msg
+*/
+
+#pragma once
+
+#include "../../includes/clcc.h"
+
+PUBLIC_PROTO_CLCC(int, msg);
---
+++ src/sys/msg.c
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2024
+** libc-calls-tester
+** File description:
+** select
+*/
+
+#include "internal.h"
+
+DECL_CLCC_ARGS_2(int, msgget, key_t, int)
