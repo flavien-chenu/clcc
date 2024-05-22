a0e73106f2c4f458a399c53f33bbb8792ee7893f
Flavien Chenu <flavienchenu@hotmail.com>
2024-05-22 23:49:39 +0200

    refactor(signal): add kill function

--- modules/signal.h
+++ modules/signal.h
@@ -14,5 +14,6 @@ __CLCC_BEGIN_DECLS
 typedef void (*sighandler_t)(int);
 
 PUBLIC_PROTO_CLCC(sighandler_t, signal)
+PUBLIC_PROTO_CLCC(int, kill)
 
 __CLCC_END_DECLS
--- src/signal.c
+++ src/signal.c
@@ -9,4 +9,5 @@
 
 typedef void (*sighandler_t)(int);
 
-DECL_CLCC_ARGS_2(sighandler_t, signal, int, sighandler_t);
+DECL_CLCC_ARGS_2(sighandler_t, signal, int, sighandler_t)
+DECL_CLCC_ARGS_2(int, kill, pid_t, int)
