f21fa67042a9eb6ce7116d29e87ab2114215bcf8
Flavien Chenu <flavienchenu@hotmail.com>
2024-06-10 23:22:52 +0200

    feat(string): add string module

---
+++ modules/string.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** libc-calls-tester
+** File description:
+** string
+*/
+
+#pragma once
+
+#include "../includes/clcc.h"
+
+__CLCC_BEGIN_DECLS
+
+PUBLIC_PROTO_CLCC(char *, strdup)
+PUBLIC_PROTO_CLCC(char *, strndup)
+
+__CLCC_END_DECLS
---
+++ src/string.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** libc-calls-tester
+** File description:
+** string
+*/
+
+#include "internal.h"
+
+DECL_CLCC_ARGS_1(char *, strdup, const char *)
+DECL_CLCC_ARGS_2(char *, strndup, const char *, size_t)
